c726ed176effbaa23f8e0500f803b76ba02dd6ae
Rakib <rakibpro98@gmail.com>
2023-05-07 21:14:33 +0600

    header and footer added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,17 +1,17 @@
-import { createBrowserRouter, } from "react-router-dom";
-import Home from "../../Pages/Home/Home";
-import Main from "../../Layout/Main";
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import Main from '../../Layout/Main';
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            }
-        ]
-    },
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+    ],
+  },
 ]);
 
 export default router;
